6ab0d308aa036f98f9bc207d6330864977264d35
Matéo <mateo.escalas@oppus.fr>
2024-03-30 12:56:06 +0100

    animation basics

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import * as THREE from "three";
+import { useRef, useState } from "react";
+import { Canvas, useFrame, useThree } from "@react-three/fiber";
+import type { Mesh } from "three";
 
-function App() {
-  const [count, setCount] = useState(0)
+function Box({ z }: { z: number }) {
+  const meshRef = useRef<Mesh>(null!);
+  const { viewport, camera } = useThree();
+  const { width, height } = viewport.getCurrentViewport(camera, [0, 0, z]);
+
+  const [data] = useState({
+    x: THREE.MathUtils.randFloatSpread(2),
+    y: THREE.MathUtils.randFloatSpread(height),
+  });
+
+  useFrame(() => {
+    meshRef.current.position.set(data.x * width, (data.y += 0.1), z);
+    if (data.y > height / 1.5) {
+      data.y = -height / 1.5;
+    }
+  });
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <mesh ref={meshRef}>
+      <boxGeometry />
+      <meshBasicMaterial color="orange" />
+    </mesh>
+  );
 }
 
-export default App
+export default function App({ count = 100 }: { count: number }) {
+  return (
+    <Canvas>
+      {Array.from({ length: count }).map((_, i) => (
+        <Box key={i} z={-i} />
+      ))}
+    </Canvas>
+  );
+}
