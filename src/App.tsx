34386b4512224f03bff64df173dce110ffe574d4
Matéo <mateo.escalas@oppus.fr>
2024-04-03 23:18:37 +0200

    text animation with `framer-motion`

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,11 @@ import styles from "./App.module.scss";
 import { MambaBrand } from "./components/MambaBrand/MambaBrand";
 import { Slider } from "./components/Slider/Slider";
 import { BottomLeft } from "./components/BottomLeft/BottomLeft";
+import { motion } from "framer-motion";
 
 export default function App() {
   const [speed, set] = useState(1);
+
   return (
     <>
       <Suspense fallback={null}>
@@ -14,13 +16,62 @@ export default function App() {
         <div className={styles.fade_in} />
       </Suspense>
       <div className={styles.title}>
-        <p>
-          <i>Mamba</i>
-        </p>
-        <p>Mentality</p>
-        <p className={styles.subtitle}>
-          In <i>React</i> & <i>Three</i>.js —
-        </p>
+        <div
+          style={{
+            overflow: "hidden",
+            height: "65px",
+            padding: "2px",
+            width: "100%",
+          }}
+        >
+          <motion.p
+            initial={{
+              y: 100,
+            }}
+            animate={{
+              y: 0,
+              transition: { delay: 1.5, duration: 1 },
+            }}
+          >
+            <i>Mamba</i>
+          </motion.p>
+        </div>
+        <div style={{ overflow: "hidden", height: "82px", padding: "2px" }}>
+          <motion.p
+            initial={{
+              y: 100,
+            }}
+            animate={{
+              y: 0,
+              transition: { delay: 1.7, duration: 1 },
+            }}
+          >
+            Mentality
+          </motion.p>
+        </div>
+        <div className={styles.subtitle}>
+          <motion.p
+            initial={{
+              y: 100,
+            }}
+            animate={{
+              y: 0,
+              transition: { delay: 1.9, duration: 1 },
+            }}
+          >
+            In <i>React</i> & <i>Three</i>.js
+          </motion.p>
+          <motion.div
+            initial={{
+              width: 0,
+            }}
+            animate={{
+              width: "30px",
+              transition: { delay: 2, duration: 1.5 },
+            }}
+            className={styles.line}
+          />
+        </div>
       </div>
       <MambaBrand />
       <Slider speed={speed} setSpeed={set} />
--- src/components/BottomLeft/BottomLeft.tsx
+++ src/components/BottomLeft/BottomLeft.tsx
@@ -1,30 +1,111 @@
 import styles from "./BottomLeft.module.scss";
+import { motion } from "framer-motion";
 
 export const BottomLeft = () => (
   <div className={styles.bottom_left_container}>
-    <p>
-      <i>Inspiration</i> and ideas
-    </p>
-    <p>
-      <i>Fundamentals</i>
-    </p>
-    <p>
-      <i>Finding</i> models
-    </p>
-    <p>
-      <i>Preparing</i> them for the web
-    </p>
-    <p>
-      <i>Displaying</i> and changing models
-    </p>
-    <p>
-      <i>Animation</i> fundamentals
-    </p>
-    <p>
-      <i>Effects</i> and making things look good
-    </p>
-    <p>
-      Preparing for <i>performance</i> and time to load
-    </p>
+    <div className={styles.animation_container}>
+      <motion.p
+        initial={{
+          y: 100,
+        }}
+        animate={{
+          y: 0,
+          transition: { delay: 2.5, duration: 0.8 },
+        }}
+      >
+        <i>Inspiration</i> and ideas
+      </motion.p>
+    </div>
+    <div className={styles.animation_container}>
+      <motion.p
+        initial={{
+          y: 100,
+        }}
+        animate={{
+          y: 0,
+          transition: { delay: 2.6, duration: 0.8 },
+        }}
+      >
+        <i>Fundamentals</i>
+      </motion.p>
+    </div>
+    <div className={styles.animation_container}>
+      <motion.p
+        initial={{
+          y: 100,
+        }}
+        animate={{
+          y: 0,
+          transition: { delay: 2.7, duration: 0.8 },
+        }}
+      >
+        <i>Finding</i> models
+      </motion.p>
+    </div>
+    <div className={styles.animation_container}>
+      <motion.p
+        initial={{
+          y: 100,
+        }}
+        animate={{
+          y: 0,
+          transition: { delay: 2.8, duration: 0.8 },
+        }}
+      >
+        <i>Preparing</i> them for the web
+      </motion.p>
+    </div>
+    <div className={styles.animation_container}>
+      <motion.p
+        initial={{
+          y: 100,
+        }}
+        animate={{
+          y: 0,
+          transition: { delay: 2.9, duration: 0.8 },
+        }}
+      >
+        <i>Displaying</i> and changing models
+      </motion.p>
+    </div>
+    <div className={styles.animation_container}>
+      <motion.p
+        initial={{
+          y: 100,
+        }}
+        animate={{
+          y: 0,
+          transition: { delay: 3, duration: 0.8 },
+        }}
+      >
+        <i>Animation</i> fundamentals
+      </motion.p>
+    </div>
+    <div className={styles.animation_container}>
+      <motion.p
+        initial={{
+          y: 100,
+        }}
+        animate={{
+          y: 0,
+          transition: { delay: 3.1, duration: 0.8 },
+        }}
+      >
+        <i>Effects</i> and making things look good
+      </motion.p>
+    </div>
+    <div className={styles.animation_container}>
+      <motion.p
+        initial={{
+          y: 100,
+        }}
+        animate={{
+          y: 0,
+          transition: { delay: 3.2, duration: 0.8 },
+        }}
+      >
+        Preparing for <i>performance</i> and time to load
+      </motion.p>
+    </div>
   </div>
 );
